67264d0bb3dafe66412f3bc9a892d5215789238e
muratavci05 <js.muratavci@gmail.com>
2025-03-14 02:17:38 +0100

    update restore

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 window.addEventListener("load", function(){
     const canvas = this.document.getElementById("canvas1");
+    const ctx = canvas.getContext("2d");
     canvas.width = 1280;
     canvas.height = 720;
 
     ctx.fillStyle = "white";
-    ctx.lineWidth = ""
+    ctx.lineWidth = 3;
+    ctx.strokeStyle = "white";
 
     class Player {
         constructor(game){
@@ -12,14 +14,17 @@ window.addEventListener("load", function(){
             this.collisionX = this.game.width * 0.5;
             this.collisionY = this.game.height * 0.5;
             this.collisionRadius = 30;
-        }
+         }
         draw(context){
             context.beginPath();
             context.arc(this.collisionX, this.collisionY, this.collisionRadius,0, Math.PI * 2);
+            context.save();
+            context.globalAlpha = 0.5;
+            context.fill();
+            context.restore();
             context.stroke();
 
-        }
-    }
+        } }
 
     class Game {
         constructor(canvas){
