f17a0ae264a63a6682968adbf82f9313a07136f2
muratavci05 <js.muratavci@gmail.com>
2025-03-15 02:10:56 +0100

    mouse control

--- script.js
+++ script.js
@@ -8,12 +8,13 @@ window.addEventListener("load", function(){
     ctx.lineWidth = 3;
     ctx.strokeStyle = "white";
 
+    //To limit certain canvas settings only to specific draw calls we can wrap that drawing code between save and restore built -in canvas methods.
     class Player {
         constructor(game){
             this.game = game;
             this.collisionX = this.game.width * 0.5;
             this.collisionY = this.game.height * 0.5;
-            this.collisionRadius = 30;
+            this.collisionRadius = 50;
          }
         draw(context){
             context.beginPath();
@@ -32,6 +33,31 @@ window.addEventListener("load", function(){
             this.width = this.canvas.width;
             this.height = this.canvas.height;
             this.Player = new Player(this);
+            this.mouse = {
+                x: this.width * 0.5,
+                y: this.height * 0.5,
+                pressed: false
+            }
+
+
+            // event listeners
+            window.addEventListener("mousedown", e=>{
+               this.mouse.x = e.offsetX;
+               this.mouse.y = e.offsetY;
+               this.mouse.pressed = true;
+            });
+            window.addEventListener("mouseup", e=>{
+                this.mouse.x = e.offsetX;
+                this.mouse.y = e.offsetY;
+                this.mouse.pressed = false;
+             });
+             window.addEventListener("mousemove", e=>{
+                this.mouse.x = e.offsetX;
+                this.mouse.y = e.offsetY;
+                console.log(this.mouse.x)
+             });
+
+
         }
         render(context){
             this.Player.draw(context);
